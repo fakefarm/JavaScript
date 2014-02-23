7a82a98020ec6634d72e3e22116545c6402e12d9
Dave Woodall <dave@hireDave.me>
2014-02-23 05:12:24 -0700

    project adam

---
+++ adam/script.js
@@ -0,0 +1,48 @@
+(function(){
+  var settings, myAction, myGoals;
+  myGoals = (function(){
+    var Goal = {
+      settings: {
+        newButton: document.getElementById('new-button'),
+        entry: document.getElementById('goal-entry'),
+        body: document.getElementById('body'),
+      },
+      init: function(theAction) {
+        settings = this.settings;
+        this.constructor(theAction);
+      },
+      constructor: function(act) {
+        act.showBox();
+        act.hideBox();
+        act.entries();
+      },
+    };
+
+    return Goal;
+  }());
+
+  myAction = (function(){
+    Action = {
+      showBox: function(){
+        settings.newButton.onclick = function(event) {
+          settings.entry.style.display="block";
+          event.stopPropagation();
+        };
+      },
+      hideBox: function() {
+        settings.body.onclick = function() {
+          settings.entry.style.display='none';
+        };
+      },
+      entries: function() {
+        settings.entry.onclick = function(event) {
+          event.stopPropagation();
+        };
+      }
+    };
+    return Action;
+  });
+
+  var myActionly = new myAction;
+  myGoals.init(myActionly);
+})();
